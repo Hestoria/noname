a3dfc903535828ccf6d8146a4b91756a7da3134e
TING-YU HSU - UBC <tommyhsu69@gmail.com>
2022-12-16 17:32:54 -0800

    connect to postgresql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RouterModule, Routes } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
@@ -8,8 +10,20 @@ const routes: Routes = [{ path: '/users', module: UserModule }];
 
 @Module({
   imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
     UserModule,
     RouterModule.register([{ path: '/api', children: routes }]),
+
   ],
   controllers: [AppController],
   providers: [AppService],
